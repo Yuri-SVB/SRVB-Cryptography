43f20944e34ce4511c8ccfc5a92e581f3b86f3ac
Yuri da Silva Villas Boas <yurisvb@gmail.com>
2017-02-06 16:02:17 -0200

    Discarded some unecessary and log-oriented precautions on io interface and added mixed random input source.

--- integers/public_msg.hpp
+++ integers/public_msg.hpp
@@ -10,15 +10,13 @@ public:
 	t_public_msg(int size = 0) : std::deque<t_public_int>(size) {}
 	friend std::istream& operator >>   (std::istream& is, t_public_msg& tpm)	{//			{	return this->mpz_class::
 		for (int i = 0, N = tpm.size(); i < N && is; i++) {
-			// YSVB_CHECK_1(is.good(), ==, true, "need to be good")
 			is >> tpm[i];
 		}
 		return is;
 	}
 	friend std::ostream& operator <<   (std::ostream& os, const t_public_msg& tpm)	{//			{	return this->mpz_class::
 		for (int i = 0, N = tpm.size(); i < N; i++) {
-			os << tpm[i].gr() << std::endl;
-			os << tpm[i].gi() << std::endl;
+			os << tpm[i];
 		}
 		return os;
 	}
